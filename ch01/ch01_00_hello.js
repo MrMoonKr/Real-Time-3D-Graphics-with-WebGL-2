b5da4158a16a2111a562f2c3445a2b5e2f934605
MrMoonKr <MrMoonKr@gmail.com>
2022-09-26 19:19:50 +0900

    ES6 Module 기반으로 코드 변경을 위해 크롬에서 동작 확인 코드 작성

---
+++ ch01/ch01_00_hello.js
@@ -0,0 +1,8 @@
+
+import * as helloModule from "../js/hello_module.js";
+
+
+console.log( 'Hello WebGL' );
+
+helloModule.print_hello();
+
---
+++ js/hello_module.js
@@ -0,0 +1,8 @@
+
+
+/**
+ * 단순 환영 문자열 컨솔에 출력
+ */
+export function print_hello() {
+    console.log( '안녕하세요' );
+}
