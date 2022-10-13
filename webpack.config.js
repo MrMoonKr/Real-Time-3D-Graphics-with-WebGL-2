08b2b90e94e188c006d70ddaa0ef92d815c687c2
MrMoonKr <MrMoonKr@gmail.com>
2022-10-13 15:20:02 +0900

    웹팩 output.filename 부분 옵션 수정하여 해당 폴더에 출력하도록 함.

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,20 @@ module.exports = {
 
     output: {
         path: __dirname + '/dist',
-        filename: '[name].bundle.js'
+        //filename: '[name].bundle.js'
+        filename: function ( pathData, assetInfo )
+        {
+            // console.log( pathData ); // https://webpack.js.org/configuration/output/#outputfilename
+            // console.log( assetInfo );  // all just {}
+            const parent   = pathData.chunk.name.substr(0,4);
+            const name     = pathData.chunk.name;
+            const ext      = '.bundle.js';
+            const fileName = path.join( parent, name + ext );
+            // console.log( fileName );
+            return fileName;
+        } 
     },
 
     devtool: 'source-map',
-    watch: true
+    //watch: true
 }
