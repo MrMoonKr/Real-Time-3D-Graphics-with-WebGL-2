e735da4da75886a3aaeb4be889c0eb00b033a14d
Mr.Moon <MrMoonKr@gmail.com>
2023-06-16 19:45:06 +0900

    ...

--- src/common/Controls.js
+++ src/common/Controls.js
@@ -1,14 +1,26 @@
 'use strict';
 
-// Abstraction over common controls for user interaction with a 3D scene
-class Controls {
+import Camera from "./Camera";
+import Scene from "./Scene";
 
-    constructor( camera, canvas, scene ) {
+// Abstraction over common controls for user interaction with a 3D scene
+class Controls 
+{
+
+    /**
+     * 
+     * @param {Camera} camera 
+     * @param {HTMLCanvasElement} canvas 
+     * @param {Scene} scene 
+     */
+    constructor( camera, canvas, scene ) 
+    {
 
         this.scene = scene;
 
         this.camera = camera;
         this.canvas = canvas;
+
         this.picker = null;
 
         this.dragging = false;
@@ -27,11 +39,25 @@ class Controls {
         this.motionFactor = 10;
         this.keyIncrement = 5;
 
-        canvas.onmousedown = event => this.onMouseDown( event );
-        canvas.onmouseup = event => this.onMouseUp( event );
-        canvas.onmousemove = event => this.onMouseMove( event );
-        window.onkeydown = event => this.onKeyDown( event );
-        window.onkeyup = event => this.onKeyUp( event );
+        canvas.onmousedown  = event => this.onMouseDown( event );
+        canvas.onmouseup    = event => this.onMouseUp( event );
+        canvas.onmousemove  = event => this.onMouseMove( event );
+        window.onkeydown    = event => this.onKeyDown( event );
+        window.onkeyup      = event => this.onKeyUp( event );
+
+        canvas.ontouchstart = ( e ) => this.onTouch( e );
+    }
+
+    /**
+     * 작성중...
+     * @param { TouchEvent } e 
+     */
+    onTouch( e )
+    {
+        let touches     = e.changedTouches ;
+        let first       = touches[0] ;
+        let type        = e.type ;
+
     }
 
     // Sets picker for picking objects
@@ -40,19 +66,26 @@ class Controls {
     }
 
     // Returns 3D coordinates
-    get2DCoords( event ) {
+    /**
+     * 
+     * @param {MouseEvent} event 
+     * @returns 
+     */
+    get2DCoords( event ) 
+    {
         let top = 0,
             left = 0,
             canvas = this.canvas;
 
-        while ( canvas && canvas.tagName !== 'BODY' ) {
-            top += canvas.offsetTop;
-            left += canvas.offsetLeft;
-            canvas = canvas.offsetParent;
+        while ( canvas && canvas.tagName !== 'BODY' )
+        {
+            top     += canvas.offsetTop;
+            left    += canvas.offsetLeft;
+            canvas  = canvas.offsetParent;
         }
 
-        left += window.pageXOffset;
-        top -= window.pageYOffset;
+        left    += window.scrollX;
+        top     -= window.scrollY;
 
         return {
             x: event.clientX - left,
@@ -60,16 +93,28 @@ class Controls {
         };
     }
 
-    onMouseUp( event ) {
+    /**
+     * canvas 에서의 마우스 업 이벤트 핸들러.
+     * @param {MouseEvent} event 
+     */
+    onMouseUp( event ) 
+    {
         this.dragging = false;
 
-        if ( !event.shiftKey && this.picker ) {
+        if ( !event.shiftKey && this.picker )
+        {
             this.picking = false;
             this.picker.stop();
         }
     }
 
-    onMouseDown( event ) {
+    /**
+     * canvas 에서의 마우스 다운 이벤트 핸들러.
+     * @param {MouseEvent} event 
+     * @returns 
+     */
+    onMouseDown( event )
+    {
         this.dragging = true;
 
         this.x = event.clientX;
@@ -86,30 +131,35 @@ class Controls {
         if ( !this.picking ) this.picker.stop();
     }
 
-    onMouseMove( event ) {
-        this.lastX = this.x;
-        this.lastY = this.y;
-
-        this.x = event.clientX;
-        this.y = event.clientY;
+    /**
+     * canvas 에서의 마우스 이동 이벤트 핸들러.
+     * @param {MouseEvent} event 
+     * @returns 
+     */
+    onMouseMove( event )
+    {
+        this.lastX  = this.x;
+        this.lastY  = this.y;
+        this.x      = event.clientX;
+        this.y      = event.clientY;
 
         if ( !this.dragging ) return;
 
-        this.ctrl = event.ctrlKey;
-        this.alt = event.altKey;
+        this.ctrl   = event.ctrlKey;
+        this.alt    = event.altKey;
 
-        const dx = this.x - this.lastX;
-        const dy = this.y - this.lastY;
+        const dx    = this.x - this.lastX;
+        const dy    = this.y - this.lastY;
 
-        if ( this.picking && this.picker.moveCallback ) {
+        if ( this.picking && this.picker.moveCallback ) 
+        {
             this.picker.moveCallback( dx, dy );
             return;
         }
 
-        if ( !this.button ) {
-            this.alt ?
-                this.dolly( dy ) :
-                this.rotate( dx, dy );
+        if ( !this.button ) 
+        {
+            this.alt ? this.dolly( dy ) : this.rotate( dx, dy );
         }
     }
 
